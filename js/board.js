0fa9fb206eb4b1c1d6124002c1cc0f292f5bde5e
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-12-08 07:26:14 -0800

    Add board that constructs the coordinates

--- js/board.js
+++ js/board.js
@@ -1,4 +1,23 @@
-function Board() {
-  this.width = 3;
-  this.height = 3;
+"use strict";
+
+function Board(width, height) {
+  this.width = width;
+  this.height = height;
+
+  this.startX = null;
+  this.startY = null;
+
+  this.spaces = [];
+  var x,y;
+  for(x = 1; x <= width; x++) {
+    this.spaces[x] = [];
+    for(y = 1; y<= height; y++){
+      this.spaces[x][y] = "( "+ x +" , " + y + " )";
+    }
+  }
+}
+
+Board.prototype.setStart = function(x,y) {
+  this.startX = x;
+  this.startY = y; 
 }
